883247e5e71d701ceede0e26fcb2cb8f10a3244a
mohamedallam13 <mohamedallam.tu@gmail.com>
2021-10-30 23:21:46 +0200

    Add Wiki API

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,3 +23,49 @@ function closeCard() {
 $(document).ready(function () {
   $(".tabs").tabs();
 });
+
+
+// Wikipedia API
+
+const WIKIPEDIA_API_END_POINT = template`https://en.wikipedia.org/api/rest_v1/page/summary/${'searchTerm'}`
+
+function getSearchResultsFromWiki() {
+  var inputEl = $("#search-input");
+  var searchTerm = inputEl.val().replace(/\s/g, "_");
+  getWikiAPI(searchTerm)
+}
+
+function getWikiAPI(searchTerm) {
+  var url = WIKIPEDIA_API_END_POINT({ searchTerm: searchTerm });
+  fetch(url)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      if (data.title == "Not found") {
+        return;
+      }
+      console.log(data);
+      populateSummary(data);
+    });
+}
+
+function populateSummary(data) {
+  var outputEl = $("#results");
+  outputEl.append([
+    $("<div>", { html: data.extract_html }),
+    $("<a>", { text: "Read more..", href: data.content_urls.desktop.page })
+  ])
+
+}
+function template(strings, ...keys) {
+  return (function (...values) {
+    let dict = values[values.length - 1] || {};
+    let result = [strings[0]];
+    keys.forEach(function (key, i) {
+      let value = Number.isInteger(key) ? values[key] : dict[key];
+      result.push(value, strings[i + 1]);
+    });
+    return result.join('');
+  });
+}
