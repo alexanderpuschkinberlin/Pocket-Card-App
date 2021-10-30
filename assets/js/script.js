fc75f51fc2612d6b9af06f7bf04162519e4b8e58
Alexander Puschkin <alexanderpuschkin@gmail.com>
2021-10-30 13:28:49 +0200

    improved checklist view with js event listner and append content info box

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,10 @@
+var rushInfo = {
+  1: {title: "Info when clicked 1", steps: ["Step1", "Step2"]},
+  2: "Info when clicked 2",
+  3: "Info when clicked 3",
+  4: "Info when clicked 4",
+}
+
 $(document).ready(function () {
   $(".sidenav").sidenav({ edge: "right" });
 });
@@ -23,3 +30,17 @@ function closeCard() {
 $(document).ready(function () {
   $(".tabs").tabs();
 });
+
+//event listner within the card view
+$("label > span").click(showRushItemInfo);
+function showRushItemInfo(event) {
+  var selectedItem = event.target.getAttribute("data-listItem");
+  var itemInfo = rushInfo[selectedItem] 
+  console.log(itemInfo);
+  var info = "";
+  for(var i = 0; i< itemInfo.steps.length; i++){
+    info += itemInfo.steps[i] + ", ";
+    var item = $("<li>").text("dummy" + itemInfo.steps[i])
+    $("#rushInfoCard").append(item);
+  }
+}
