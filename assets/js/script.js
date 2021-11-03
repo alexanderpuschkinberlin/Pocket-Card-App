27f4b2cf835e837f36400ba5c9f135e020161c69
mohamedallam13 <mohamedallam.tu@gmail.com>
2021-11-03 22:49:02 +0100

    Add JS comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -129,6 +129,7 @@ function toggleSpinner() {
 
 const WIKIPEDIA_API_END_POINT = template`https://en.wikipedia.org/api/rest_v1/page/summary/${"searchTerm"}`;
 
+// Getting results from wikipedia main function 
 function getSearchResultsFromWiki() {
   var inputEl = $("#search-input");
   var searchTerm = inputEl.val().replace(/\s/g, "_");
@@ -139,6 +140,7 @@ function getSearchResultsFromWiki() {
   getWikiAPI(searchTerm);
 }
 
+// checking first in local storage and if found then populate the results div
 function checkInLocalStorage(searchTerm) {
   searchTerm = searchTerm.toUpperCase();
   var storedResult = localStorage.getItem(searchTerm);
@@ -149,6 +151,7 @@ function checkInLocalStorage(searchTerm) {
   }
 }
 
+// fetch from wiki if not found in local storage, and populate the results div
 function getWikiAPI(searchTerm) {
   var url = WIKIPEDIA_API_END_POINT({ searchTerm: searchTerm });
   fetch(url)
@@ -165,12 +168,15 @@ function getWikiAPI(searchTerm) {
     });
 }
 
+// adding to local storage
 function addToLocalStorage(searchTerm, data) {
   searchTerm = searchTerm.toUpperCase();
   var stringData = JSON.stringify(data);
   localStorage.setItem(searchTerm, stringData);
 }
 
+
+// populating the results div and adding the read more link
 function populateSummary(data) {
   var outputEl = $("#wiki-results");
   outputEl.html("");
@@ -188,6 +194,7 @@ function populateSummary(data) {
 
 const COVID_API_ENDPOINT = template`https://covid19-eu-data-api-gamma.now.sh/api/countries?alpha2=${"countryCode"}&days=${"days"}`;
 
+// Equivalent Table Header
 const HEADERS_MAP = {
   cases: "Cases",
   "cases/100k pop.": "Cases /100k Pop",
@@ -198,11 +205,13 @@ const HEADERS_MAP = {
   intensive_care: "Intensive Care",
 };
 
+// Max count of table columns allowed in the results
 const COLUMNS_LIMIT = 5;
 
+// On loading of page, table clears
 clearTableDiv();
-//getCOVIDInfo();
 
+// Fetching Results Main Function
 function getCOVIDInfo() {
   var countryCode = getSelectedCountry() || "de";
   var days = getDaysBack();
@@ -212,6 +221,7 @@ function getCOVIDInfo() {
   getCOVIDAPIInfo(days, countryCode);
 }
 
+// Getting how far back the results are desired to be from latest data date
 function getDaysBack() {
   var daysBackInput = $("#days-back");
   var daysBack = daysBackInput.val();
@@ -222,6 +232,8 @@ function getDaysBack() {
   return daysBack;
 }
 
+
+// Getting selected country from dropdown
 function getSelectedCountry() {
   var selectionDiv = $("#country-selection");
   var selectedCountry = selectionDiv.val();
@@ -236,7 +248,9 @@ function clearDataDate() {
   $("#date-of-data").text("");
 }
 
+// Fetching data from API
 function getCOVIDAPIInfo(days, countryCode) {
+  // Template Literal to replace place holders in the end point
   var url = COVID_API_ENDPOINT({ countryCode: countryCode, days: days });
   fetch(url)
     .then(function (response) {
@@ -255,6 +269,7 @@ function setDataDate(data) {
   $("#date-of-data").text("Date of Data: " + data.date);
 }
 
+// Table main populating function
 function populateTable(data) {
   var records = data.records;
   var headers = Object.keys(records[0]);
@@ -263,18 +278,21 @@ function populateTable(data) {
   var tableDiv = $("#covid-results");
   var tableHeaderDiv = $("<thead>", { class: "red lighten-4" });
   var tableBodyDiv = $("<tbody>");
+  // Create Table Header
   var headersDiv = createTableRowDiv(headers, HEADERS_MAP, "<th>");
   tableHeaderDiv.append(headersDiv);
-  console.log(locationBase);
+  // Create Table Results Rows 
   records.forEach((record) => {
     var recordRowDiv = createTableRowDiv(headers, record, "<td>", HEADERS_MAP);
     tableBodyDiv.append(recordRowDiv);
   });
   tableDiv.append(tableHeaderDiv);
   tableDiv.append(tableBodyDiv);
+  // to make sure that the next fetch will not diplay several geographical locations from last results as location columns
   delete HEADERS_MAP[locationBase];
 }
 
+// Row creation function, gets type of row (header or entry) and gets the entry and creates the columns elements one by one and appends it to the row
 function createTableRowDiv(headers, entry, type, reference) {
   reference = reference || entry;
   var tableRowDiv = $("<tr>");
@@ -291,6 +309,7 @@ function createTableRowDiv(headers, entry, type, reference) {
   return tableRowDiv;
 }
 
+// Values cleaning, to ensure the data looks more human readable
 function proper(value) {
   if (typeof value == "string") {
     return toTitleCase(value);
@@ -301,6 +320,7 @@ function proper(value) {
   }
 }
 
+// Get which geographic location is to be used as a location for each row
 function getLocationBase(headers) {
   if (headers.includes("lau")) {
     return "lau";
@@ -316,6 +336,7 @@ function getLocationBase(headers) {
   }
 }
 
+// template literal replacement of placeholders from a provided object
 function template(strings, ...keys) {
   return function (...values) {
     let dict = values[values.length - 1] || {};
@@ -328,6 +349,7 @@ function template(strings, ...keys) {
   };
 }
 
+// convert to proper / title case
 function toTitleCase(str) {
   return str.replace(/\w\S*/g, function (txt) {
     return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
